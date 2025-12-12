e4a08ef3e4669f34a48f14283a04bab61c4c2bb9
Pranav <codewithpranav.tech@gmail.com>
2025-12-12 17:11:55 +0530

    req.params and query

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ app.use("/test",(req,res)=>{
     res.end("hello namasthe server")
 })
 
+app.get("/use/:id",(req,res)=>{
+    console.log(req.params);
+    res.send(`your id is ${req.params.id}`)
+})
+
+app.get("/search",(req,res)=>{
+    console.log(req.query);
+    res.send(req.query)
+})
 
 app.get("/user",(req,res)=>{
     res.send({firstname:"Akshai",Lastname:"Saini"})
@@ -17,6 +26,10 @@ app.post("/user",(req,res)=>{
 
 app.delete("/user",(req,res)=>{
    res.send("data deleted sucessfully") 
+})
+
+app.get(/a/,(req,res)=>{
+   res.send("chck the a") 
 })
 app.listen(3000,()=>{
     console.log("server running")
