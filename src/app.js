3670baec61aadd7c5e77e02f646871822cbe48a4
Pranav <codewithpranav.tech@gmail.com>
2025-12-17 13:11:37 +0530

    middlware functioning

--- src/app.js
+++ src/app.js
@@ -1,35 +1,20 @@
-const express=require("express")
+  const express=require("express")
 
 const app=express();
 
-app.use("/test",(req,res)=>{
-    res.end("hello namasthe server")
-})
 
-app.get("/use/:id",(req,res)=>{
-    console.log(req.params);
-    res.send(`your id is ${req.params.id}`)
-})
+app.use("/user",(req,res,next)=>{
+    console.log("middleware one running");
+    next()
+});
 
-app.get("/search",(req,res)=>{
-    console.log(req.query);
-    res.send(req.query)
-})
+app.get("/user",(req,res,next)=>{
+    console.log("middleware 2 running");
+    next();
+},
 
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Akshai",Lastname:"Saini"})
-})
-
-app.post("/user",(req,res)=>{
-    res.send("data saved sucessfully")
-})
-
-app.delete("/user",(req,res)=>{
-   res.send("data deleted sucessfully") 
-})
-
-app.get(/a/,(req,res)=>{
-   res.send("chck the a") 
+(req,res,next)=>{
+res.send("Router working")
 })
 app.listen(3000,()=>{
     console.log("server running")
