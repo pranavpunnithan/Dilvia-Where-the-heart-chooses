8a1bec2a439b2275b7d1ec1c945f3476c2544894
Pranav <codewithpranav.tech@gmail.com>
2025-12-24 22:52:04 +0530

    Date added to database

--- src/app.js
+++ src/app.js
@@ -4,7 +4,22 @@ require("./config/database");
 const connectDb=require("./config/database")
 
 const app= express();
+const User=require("./models/user");
 
+app.post("/signup",async(req,res)=>{
+    const userObj={
+        firstName:"MS",
+        lastName:"Dhoni",
+        emailId:"MSD@gmail.com",
+        password:"Msd@123"
+    }
+
+const user=new User(userObj);
+//CREATING A NEW INSTATNCE OF THE USER MODEL
+await user.save();
+//.save returns a promise hence we have to use async ,await
+res.send("User Added Sucessfully")
+});
 
 connectDb()
 .then(()=>{
